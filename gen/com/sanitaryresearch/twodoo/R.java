69f2d3a3292defa2abe86bbbf7548288ddd6bd6d
Mike Selender <mike_selender@mindspring.com>
2013-02-06 08:25:54 -0500

    More orefs work.

--- gen/com/sanitaryresearch/twodoo/R.java
+++ gen/com/sanitaryresearch/twodoo/R.java
@@ -9,9 +9,14 @@ package com.sanitaryresearch.twodoo;
 
 public final class R {
     public static final class array {
-        public static final int list_defaults=0x7f060002;
-        public static final int list_names=0x7f060001;
-        public static final int list_types=0x7f060000;
+        public static final int bartleby=0x7f060005;
+        public static final int candoo=0x7f060004;
+        public static final int cantdoo=0x7f060003;
+        public static final int hamlet=0x7f060006;
+        public static final int prefs_index=0x7f060007;
+        public static final int twodo=0x7f060001;
+        public static final int twodoo=0x7f060000;
+        public static final int undoo=0x7f060002;
     }
     public static final class attr {
     }
@@ -47,24 +52,61 @@ public final class R {
         public static final int app_description=0x7f070004;
         public static final int app_long_name=0x7f070003;
         public static final int app_name=0x7f070000;
-        public static final int blank_summary=0x7f070014;
-        public static final int candoo_title=0x7f07000e;
-        public static final int candoo_type=0x7f070013;
-        public static final int cantdoo_title=0x7f07000d;
-        public static final int cantdoo_type=0x7f070012;
-        public static final int dontdoo_title=0x7f07000b;
-        public static final int dontdoo_type=0x7f070010;
+        public static final int bartleby_array=0x7f070038;
+        public static final int bartleby_default=0x7f070031;
+        public static final int bartleby_icon=0x7f07002a;
+        public static final int bartleby_key=0x7f070015;
+        public static final int bartleby_summary=0x7f070023;
+        public static final int bartleby_title=0x7f07001c;
+        public static final int candoo_array=0x7f070037;
+        public static final int candoo_default=0x7f070030;
+        public static final int candoo_icon=0x7f070029;
+        public static final int candoo_key=0x7f070014;
+        public static final int candoo_summary=0x7f070022;
+        public static final int candoo_title=0x7f07001b;
+        public static final int cantdoo_array=0x7f070036;
+        public static final int cantdoo_default=0x7f07002f;
+        public static final int cantdoo_icon=0x7f070028;
+        public static final int cantdoo_key=0x7f070013;
+        public static final int cantdoo_summary=0x7f070021;
+        public static final int cantdoo_title=0x7f07001a;
+        public static final int dontdoo_array=0x7f070034;
+        public static final int dontdoo_default=0x7f07002d;
+        public static final int dontdoo_icon=0x7f070026;
+        public static final int dontdoo_key=0x7f070011;
+        public static final int dontdoo_summary=0x7f07001f;
+        public static final int dontdoo_title=0x7f070018;
+        public static final int enterprise_group_key=0x7f07000b;
+        public static final int enterprise_group_title=0x7f07000e;
+        public static final int hamlet_array=0x7f070039;
+        public static final int hamlet_default=0x7f070032;
+        public static final int hamlet_icon=0x7f07002b;
+        public static final int hamlet_key=0x7f070016;
+        public static final int hamlet_summary=0x7f070024;
+        public static final int hamlet_title=0x7f07001d;
         public static final int hello_world=0x7f070001;
+        public static final int literary_group_key=0x7f07000c;
+        public static final int literary_group_title=0x7f07000f;
         public static final int menu_settings=0x7f070002;
         public static final int new_list_button_text=0x7f070007;
         public static final int new_list_dialog_title=0x7f070008;
-        public static final int settings_label=0x7f070015;
-        public static final int settings_shortcut=0x7f070017;
-        public static final int settings_title=0x7f070016;
-        public static final int twodoo_title=0x7f07000a;
-        public static final int twodoo_type=0x7f07000f;
-        public static final int undoo_title=0x7f07000c;
-        public static final int undoo_type=0x7f070011;
+        public static final int settings_label=0x7f07003a;
+        public static final int settings_shortcut=0x7f07003c;
+        public static final int settings_title=0x7f07003b;
+        public static final int standard_group_key=0x7f07000a;
+        public static final int standard_group_title=0x7f07000d;
+        public static final int twodoo_array=0x7f070033;
+        public static final int twodoo_default=0x7f07002c;
+        public static final int twodoo_icon=0x7f070025;
+        public static final int twodoo_key=0x7f070010;
+        public static final int twodoo_summary=0x7f07001e;
+        public static final int twodoo_title=0x7f070017;
+        public static final int undoo_array=0x7f070035;
+        public static final int undoo_default=0x7f07002e;
+        public static final int undoo_icon=0x7f070027;
+        public static final int undoo_key=0x7f070012;
+        public static final int undoo_summary=0x7f070020;
+        public static final int undoo_title=0x7f070019;
     }
     public static final class style {
         /** 
